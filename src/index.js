00f06dc0ae2ea2cbcc1bc45ae8fe06d6a744b6fc
Anmol53 <anmol.ag53@gmail.com>
2020-12-12 18:50:52 +0530

    Try 2

--- src/index.js
+++ src/index.js
@@ -9,7 +9,22 @@ app.use(express.json());
 const { connections } = require("mongoose");
 const { connection } = require("./connector");
 
-const filterMW = (req, res, next) => {
+// const filterMW = (req, res, next) => {
+//   req.query.minPackage = !isNaN(req.query.minPackage)
+//     ? parseInt(req.query.minPackage)
+//     : 0;
+//   req.query.maxFees = !isNaN(req.query.maxFees)
+//     ? parseInt(req.query.maxFees)
+//     : 25;
+//   req.query.name = req.query.name ? req.query.name : "";
+//   req.query.state = req.query.state ? req.query.state : "";
+//   req.query.city = req.query.city ? req.query.city : "";
+//   req.query.isCourse = req.query.course ? true : false;
+//   req.query.isExam = req.query.exam ? true : false;
+//   next();
+// };
+
+app.get("/findColleges", async (req, res) => {
   req.query.minPackage = !isNaN(req.query.minPackage)
     ? parseInt(req.query.minPackage)
     : 0;
@@ -21,10 +36,6 @@ const filterMW = (req, res, next) => {
   req.query.city = req.query.city ? req.query.city : "";
   req.query.isCourse = req.query.course ? true : false;
   req.query.isExam = req.query.exam ? true : false;
-  next();
-};
-
-app.get("/findColleges", filterMW, async (req, res) => {
   connection
     .aggregate()
     .addFields({ isCourse: req.query.isCourse, isExam: req.query.isExam })
